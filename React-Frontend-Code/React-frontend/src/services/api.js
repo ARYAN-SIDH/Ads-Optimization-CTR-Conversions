3c45119243c402dc2b8cd4b979b7449fe8bf97ce
ARYAN-SIDH <144741627+ARYAN-SIDH@users.noreply.github.com>
2025-05-04 21:30:15 -0400

    React-Frontend
    React Frontend Code and static files used is being imported.

---
+++ React-Frontend-Code/React-frontend/src/services/api.js
@@ -0,0 +1,18 @@
+const API_KEY = "3d9a3659825027dd08a3658c1e3eccde";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  const data = await response.json();
+  return data.results;
+};
+
+export const searchMovies = async (query) => {
+  const response = await fetch(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+      query
+    )}`
+  );
+  const data = await response.json();
+  return data.results;
+};
